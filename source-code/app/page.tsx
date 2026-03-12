edb1c33dfce48dfd7e7f10c6c409a0a73c826081
michal92299 <chudybyczek2137@gmail.com>
2026-03-12 19:29:36 +0100

    Create page.tsx

---
+++ source-code/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import ScreenUI from '@/components/ScreenUI';
+
+export default function Page() {
+  return (
+    <main className="h-screen w-screen overflow-hidden bg-[#050510]">
+      <ScreenUI />
+    </main>
+  );
+}
